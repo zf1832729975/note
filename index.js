ecefd863acaf31828dc7463b4838b565cf8148b9
zhoufei <1832729975@qq.com>
2019-10-04 14:40:35 +0800

    2019-10-4 2:40:34 PM

--- index.js
+++ index.js
@@ -52,9 +52,8 @@ var date = new Date().toLocaleString()
 cmd.exec(`git pull`)
   .then(() => cmd.exec(`git add . && git commit -m "${date}"`))
   .then(() => cmd.exec(`git push --all`))
+  .then(() => cmd.exec(`pause`))
   .then(() => {
     console.log(style.greenBG, 'DONE')
   })
-  .then(() => cmd.exec(`pause`))
-  
   .catch(() => {})
